00aeffa08a80a785b380bb3e8bd289094aa087a3
CR0NYM3X <88253386+CR0NYM3X@users.noreply.github.com>
2026-02-23 12:47:24 -0700

    Update pg_generate_rollback.sql

--- SQL/pg_generate_rollback.sql
+++ SQL/pg_generate_rollback.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE FUNCTION public.pg_generate_rollback(
+CREATE SCHEMA IF NOT EXISTS audit;
+
+CREATE OR REPLACE FUNCTION audit.pg_generate_rollback(
     p_audit_table text,
     p_id_log      bigint
 )
